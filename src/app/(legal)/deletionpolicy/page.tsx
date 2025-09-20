5910837f771003092820c188161b9c76243edae4
jiyong <parkjiy96@naver.com>
2025-09-20 15:09:16 +0900

    이용약관 추가

--- src/app/(legal)/deletionpolicy/page.tsx
+++ src/app/(legal)/deletionpolicy/page.tsx
@@ -1,5 +1,54 @@
+"use client";
+import styles from "./deletionpolicyPage.module.scss";
+import classNames from "classnames/bind";
+
+const cn = classNames.bind(styles);
+
 const DeletionpolicyPage = () => {
-  return <div>여기는 삭제약관</div>;
+  return (
+    <div className={cn("container")}>
+      <h1>계정 및 데이터 삭제 정책</h1>
+      <h2>1. 계정 삭제와 관련된 내용</h2>
+      <p>계정을 삭제하려면 앱을 실행하고 다음 단계를 따르세요:</p>
+      <ol>
+        <li>1. 프로필 페이지로 이동합니다.</li>
+        <li>
+          2. <strong>계정 삭제</strong>를 클릭합니다.
+        </li>
+        <li>
+          3. 계정 삭제 페이지에서 계정 삭제 이유를 선택한 후 삭제 버튼을
+          클릭합니다.
+        </li>
+        <li>4. 삭제 확인 메시지에 동의하여 계정을 삭제할 수 있습니다.</li>
+      </ol>
+      <p>
+        계정 삭제 시, 데이터는 즉시 삭제되며, 법적으로 요구되는 경우 일정 기간
+        보관될 수 있습니다.
+      </p>
+      <h2>2. 데이터 삭제 요청 방법</h2>
+      <p>계정을 삭제하지 않고 데이터 삭제를 요청하려면 다음 단계를 따르세요:</p>
+      <ul>
+        <li>1. 자신이 올린 게시물을 삭제할 수 있는 기능을 사용하세요.</li>
+        <li>
+          2. 삭제하려는 게시물에서 <strong>삭제</strong> 버튼을 클릭한 후, 확인
+          메시지에 동의하여 삭제를 완료하세요.
+        </li>
+      </ul>
+      <br />
+      <strong>문의하기</strong>
+      <p>
+        계정 및 데이터 삭제 정책에 관한 질문이나 제안 사항이 있으시면
+        <a
+          href="mailto:dapjiofficial@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          dapjiofficial@gmail.com
+        </a>
+        으로 서비스 제공자에게 문의해 주시기 바랍니다.
+      </p>
+    </div>
+  );
 };
 
 export default DeletionpolicyPage;
--- src/components/common/homeButtons/index.tsx
+++ src/components/common/homeButtons/index.tsx
@@ -7,25 +7,27 @@ type Item = {
   id: number;
   name: "admin" | "privacy" | "terms" | "deletionpolicy";
   route: string;
+  target?: "_blank" | "_self";
 };
 
 const buttons: Item[] = [
   { id: 1, name: "admin", route: "/admin/login" },
-  { id: 2, name: "privacy", route: "/privacy" },
-  { id: 3, name: "terms", route: "/terms" },
-  { id: 4, name: "deletionpolicy", route: "/deletionpolicy" },
+  { id: 2, name: "privacy", route: "/privacy", target: "_blank" },
+  { id: 3, name: "deletionpolicy", route: "/deletionpolicy", target: "_blank" },
+  { id: 4, name: "terms", route: "/terms", target: "_blank" },
 ];
 
 export default function HomeButtons() {
   return (
     <div className={cn("container")}>
-      {buttons.map(({ id, name, route }) => (
+      {buttons.map(({ id, name, route, target }) => (
         <Link
           key={id}
           href={route}
           prefetch
           className={cn("btn", { accent: name === "admin" })}
           aria-label={name}
+          target={target ?? "_self"}
         >
           {name}
         </Link>
